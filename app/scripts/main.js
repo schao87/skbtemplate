4beee8091cd02ebe8f9230ff6f497c69fa4fb440
schao87 <schaosti@gmail.com>
2015-04-23 19:36:34 -0400

    hover effects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	$(window).on('resize', function(){
+		var win = $(this);
+		if (win.width()>768){
+			$('.thumbnailTitle').css('display','none');
+			$('.thumbnail').on('mouseover', function(){
+				$(this).find('.thumbnailTitle').css('display','block');
+			})
+			$('.thumbnail').on('mouseout', function(){
+				$(this).find('.thumbnailTitle').css('display','none');
+			})
+		}else if(win.width()<768){
+			$('.thumbnailTitle').css('display','block');
+			$('.thumbnail').off('mouseover');
+			$('.thumbnail').off('mouseout');
+		}
+	})
+		if ($(window).width()>768){
+			$('.thumbnailTitle').css('display','none');
+			$('.thumbnail').on('mouseover', function(){
+				$(this).find('.thumbnailTitle').css('display','block');
+			})
+			$('.thumbnail').on('mouseout', function(){
+				$(this).find('.thumbnailTitle').css('display','none');
+			})
+		}else if($(window).width()<768){
+			$('.thumbnailTitle').css('display','block');
+			$('.thumbnail').off('mouseover');
+			$('.thumbnail').off('mouseout');
+		}
+})
